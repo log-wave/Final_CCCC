25009ba822f0124ae4ea4d877f3a7a9a3ce73978
yoon <yoon@DESKTOP-TKFAQC9>
2021-07-05 12:36:28 +0900

    2021-07-01_답변기능2

--- src/main/java/com/kh/Final_cccc/board/service/BoardService.java
+++ src/main/java/com/kh/Final_cccc/board/service/BoardService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.kh.Final_cccc.board.model.vo.Board;
 import com.kh.Final_cccc.board.model.vo.PageInfo;
+import com.kh.Final_cccc.board.model.vo.QA_Answer;
 //다시
 public interface BoardService{
 
@@ -31,5 +32,8 @@ public interface BoardService{
 
 	int deleteqaBoard(int bNo);
 
+	int insertAnswer(QA_Answer qa);
+
+	ArrayList<QA_Answer> selectAnswerList(int bNo);
 
 }
