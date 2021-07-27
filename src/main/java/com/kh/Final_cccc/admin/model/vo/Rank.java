de20ad052a7c022ea77a9cd9922ab610da2e192f
mingi <mingi0325@gmail.com>
2021-07-27 10:26:52 +0900

    21.07.27 서민기 vo추가 커밋

---
+++ src/main/java/com/kh/Final_cccc/admin/model/vo/Rank.java
@@ -0,0 +1,40 @@
+package com.kh.Final_cccc.admin.model.vo;
+
+public class Rank {
+	private String Name;
+    private int count;
+    private int Rank;
+    public Rank() {}
+
+    public Rank(String name, int count, int rank) {
+        super();
+        Name = name;
+        this.count = count;
+
+        Rank = rank;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getRank() {
+        return Rank;
+    }
+
+    public void setRank(int rank) {
+        Rank = rank;
+    }
+}
